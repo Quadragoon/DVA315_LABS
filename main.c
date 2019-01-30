7d9198e8915841342621dc789f95e942ec93b7b6
quadragoon <joel.risberg@gmail.com>
2019-01-30 12:42:36 +0100

    Updated files up until lab assignment 4a)

--- main.c
+++ main.c
@@ -1,6 +1,63 @@
 #include "wrapper.h"
+#include <zconf.h>
+#include <semaphore.h>
+
+#define MAX_SIZE 1024
+
+void* helloWorld(int* count);
+void* helloMoon(int* count);
+void* readText();
+
+sem_t mutex;
 
 int main(int ac, char ** argv) {
-    pthread_t * hej;
-    printf("Hello world!");
+    pthread_t readThread;
+
+    pthread_create(&readThread, NULL, readText, NULL);
+
+    mqd_t mailbox;
+    int connected = 0;
+    while (!connected) {
+        connected = MQconnect(&mailbox, "/Mailbox");
+        usleep(50);
+    }
+
+    sem_init(&mutex, 0, 1);
+
+    char* textString = malloc(sizeof(char)*MAX_SIZE);
+    while(1) {
+        printf("Enter string: ");
+        sem_wait(&mutex);
+        fgets(textString, MAX_SIZE, stdin);
+        textString[strlen(textString) - 1] = '\0';
+        MQwrite(&mailbox, textString);
+        sem_post(&mutex);
+        if (strcmp(textString, "END") == 0)
+            pthread_exit(NULL);
+        sleep(1);
+    }
+}
+
+void* readText() {
+    mqd_t mailbox;
+
+    int success = 0;
+    while (!success) {
+        success = MQcreate(&mailbox, "/Mailbox");
+    }
+
+    void* textString = malloc(MAX_SIZE);
+
+    while (1) {
+        sleep(1);
+        sem_wait(&mutex);
+        if (!MQread(&mailbox, &textString))
+            printf("Read failed: error %d, %s\n", errno, strerror(errno));
+        else {
+            printf("%s\n", (char *) textString);
+        }
+        sem_post(&mutex);
+        if (strcmp(textString, "END") == 0)
+            pthread_exit(NULL);
+    }
 }
--- wrapper.c
+++ wrapper.c
@@ -11,11 +11,21 @@ int MQcreate (mqd_t * mq, char * name)
     /* Should be able to handle a messages of any size */
     /* Should return 1 on success and 0 on fail*/
     mqd_t mailslot;
-    mailslot = mq_open(name, O_RDWR|O_CREAT|O_EXCL, 644, NULL);
-    if (mailslot == -1)
+
+    struct mq_attr attributes;
+
+    attributes.mq_flags = 0;
+    attributes.mq_maxmsg = 10;
+    attributes.mq_msgsize = MAX_SIZE;
+    attributes.mq_curmsgs = 0;
+
+    mailslot = mq_open(name, O_CREAT | O_RDWR | O_NONBLOCK, 0777, &attributes);
+    if (mailslot == -1) {
+        printf("ERROR in MQcreate: ERROR CODE %d: %s\n", errno, strerror(errno));
         return 0;
+    }
     else {
-        mq = &mailslot;
+        *mq = mailslot;
         return 1;
     }
 }
@@ -27,11 +37,11 @@ int MQconnect (mqd_t * mq, char * name)
     /* Should return 1 on success and 0 on fail*/
 
     mqd_t mailslot;
-    mailslot = mq_open(name, O_RDWR);
+    mailslot = mq_open(name, O_RDWR | O_NONBLOCK);
     if (mailslot == -1)
         return 0;
     else {
-        mq = &mailslot;
+        *mq = mailslot;
         return 1;
     }
 }
@@ -40,31 +50,35 @@ int MQread (mqd_t * mq, void ** refBuffer)
 {
     /*  Uses mq as reference pointer, so that you can 		reach the handle from anywhere */
     /* Read a msg from a mailslot, return nr */
-    /* of successful bytes read              */
+    /* of successful bytes read            */
 
-    int retval = mq_receive(*mq, (char*)*refBuffer, 8192, NULL);
-    if (retval > 0)
+    int retval = mq_receive(*mq, *refBuffer, MAX_SIZE, NULL);
+    if (retval >= 0) {
+        //printf("MQread success, %d bytes read\n", retval);
         return retval;
+    }
     else
         return 0;
 }
 
 int MQwrite (mqd_t * mq, void * sendBuffer)
 {
-    /*  Uses mq as reference pointer, so that you can 	     reach the handle from anywhere */
+    /* Uses mq as reference pointer, so that you can reach the handle from anywhere */
     /* Write a msg to a mailslot, return nr */
-    /* of successful bytes written         */
+    /* of successful bytes written */
     int length = 0;
     char* message = (char*)sendBuffer;
     while(message[length] != '\0') {
         length++;
     }
+    length++;
 
     int retval;
-    //mq_send(mq, sendBuffer, sizeof(char) * length, )
-    retval = mq_send(*mq, sendBuffer, sizeof((char*)sendBuffer), 1);
-    if (retval == 0)
-        return sizeof((char*)sendBuffer);
+    retval = mq_send(*mq, sendBuffer, (int)sizeof(char) * length, 0);
+    if (retval == 0) {
+        //printf("MQwrite success, %d bytes written\n", (int)sizeof(char) * length);
+        return (int)sizeof(char) * length;
+    }
     else
         return 0;
 }
@@ -74,13 +88,23 @@ int MQclose(mqd_t * mq, char * name)
     /*  Uses mq as reference pointer, so that you can reach the handle from anywhere */
     /* close a mailslot, returning whatever the service call returns */
     /* Should return 1 on success and 0 on fail*/
-    return 0;
+    int retval;
+    retval = mq_close(*mq);
+    if (retval == 0)
+        return 1;
+    else
+        return 0;
 }
+
 int threadCreate (void * functionCall, int threadParam)
 {
 	/* Creates a thread running threadFunc */
 	/* Should return 1 on success and 0 on fail*/
-	return 0;
+	/*int retval;
+	pthread_t thread;
+	retval = pthread_create(&thread, NULL, functionCall, (void*)&threadParam);
+	if (retval == 0)
+	    return 1;
+	else*/
+	    return 0;
 }
-
-
